4d98096dc5b4226183b8a57444228c7f3bb64e48
Nhlanhla-dev <nsx.xulu@gmail.com>
2023-05-04 09:44:23 +0200

    Added the functionality for the FizzBuzz method.

--- Testing_1/src/main.java
+++ Testing_1/src/main.java
@@ -8,17 +8,26 @@
  */
 public class main {
 
-	private void FizzBuzz(int number)
+	private static void FizzBuzz(int number)
 	{
-		
+		if(number%15==0 && number%3==0)
+		{
+			System.out.println("Fizz Buzz");
+		}else if(number%5==0) 
+		{
+			System.out.println("Buzz");
+		}else if(number%3==0)
+		{
+			System.out.println("Fizzz");
+		}
 	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World 3");
-
+		int number =15;
+		FizzBuzz(number);
 	}
 
 }
